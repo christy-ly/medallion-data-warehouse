f15c5fe310f8109b5f2d475e2263b4c696d9b292
DESKTOP-TFBL5RQ\Christy <christy27.lee@gmail.com>
2026-02-10 11:13:18 -0500

    Add SQL script for Gold layer data modeling

---
+++ scripts/gold/load_tables/gold_load_customers.sql
@@ -0,0 +1,23 @@
+CREATE VIEW
+    gold.dim_customers AS
+SELECT
+    ROW_NUMBER() OVER (
+        ORDER BY
+            cst_id
+    ) AS customer_key,
+    ci.cst_id AS customer_id,
+    ci.cst_key AS customer_number,
+    ci.cst_firstname AS first_name,
+    ci.cst_lastname AS last_name,
+    la.cntry AS country,
+    CASE
+        WHEN ci.cst_gndr != 'N/A' THEN ci.cst_gndr
+        ELSE COALESCE(ca.gen, 'N/A')
+    END AS gender,
+    ci.cst_marital_status AS marital_status,
+    ca.bdate AS birthdate,
+    cst_create_date AS create_date
+FROM
+    silver.crm_cust_info ci
+    LEFT JOIN silver.erp_cust_az12 ca ON ci.cst_key = ca.cid
+    LEFT JOIN silver.erp_loc_a101 la ON ci.cst_key = la.cid
